092fe6bf9bbd10b304490f4b1ce374f8fc9bfbda
KahlilJoshua <136188721+KahlilJoshua@users.noreply.github.com>
2023-09-15 20:49:23 +0200

    Add files via upload

---
+++ Malithong Thai/script.js
@@ -0,0 +1,7 @@
+// Toggle the navigation links when the hamburger menu is clicked
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+hamburgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});

35293f6b8c0fb8080120cf0ae4d2017626c64349
KahlilJoshua <136188721+KahlilJoshua@users.noreply.github.com>
2023-09-19 15:56:09 +0200

    Update script.js

--- Malithong Thai/script.js
+++ Malithong Thai/script.js
@@ -1,4 +1,4 @@
-// Toggle the navigation links when the hamburger menu is clicked
+
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navLinks = document.querySelector('.nav-links');
 
